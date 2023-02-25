fa9a1f6fd2159485bb51254d0aab6d3788fe1753
abdelkader5 <Abdelkader.belhadj@esprit.tn>
2023-02-25 01:06:49 +0100

    finishing ALL Tasks

--- seance 5/models/student.js
+++ seance 5/models/student.js
@@ -2,9 +2,19 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 var Student = new Schema ({
-    Name : String,
-    Age : Number,
-    Note : Number
+    Name:{
+        type:String,
+        unique:true,
+        required:true
+    },
+    Age:{
+        type:Number,
+        required:true
+    },
+    Note:{
+        type:Number,
+        required:true
+    }
 });
 
 module.exports = mongoose.model('students',Student);
--- seance 5/routes/student.js
+++ seance 5/routes/student.js
@@ -1,16 +1,21 @@
 const express = require("express");
 const router = express.Router();
 var Student = require("../models/student.js");
-const { getStudent, saveStudent , deleteStudent , updateStudent , searchStudent ,searchStudentByName,displayAge,displayStudent} = require("../services/student.js");
+const { getStudent, saveStudent , deleteStudent , updateStudent ,studentsNoteSorted, searchStudent,deleteStudentName ,searchStudentByName,displayAge,displayStudent} = require("../services/student.js");
 
 router.get('/',getStudent);
 router.post('/',saveStudent);
-router.delete('/delete/:id',deleteStudent);
-router.put('/:id',updateStudent);
+//router.delete('/delete/:id',deleteStudent);
+router.put('/update',updateStudent);
+//router.put('/:id',updateStudent);
 // router.get('/find/:id',searchStudent);
 router.get('/find/:name',searchStudentByName);
 router.get('/getage',displayAge);
-router.post('/filterStudent',displayStudent);
+router.put('/filterStudent',displayStudent);
+router.delete('/delete/:name',deleteStudentName);
+router.get('/studentsNoteSorted',studentsNoteSorted)
+
+
 
 
 
