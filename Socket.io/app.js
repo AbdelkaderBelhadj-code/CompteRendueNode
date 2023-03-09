2fc1a5dbe6c7d9135fea9600bc26e5bf32c562e6
abdelkader5 <Abdelkader.belhadj@esprit.tn>
2023-03-09 20:56:43 +0100

    isTyping Done

--- Socket.io/app.js
+++ Socket.io/app.js
@@ -1,26 +1,31 @@
-const express = require("express");
-const http = require("http");
-var path = require("path");
-const chatRouter = require("./routes/chat")
+    const express = require("express");
+    const http = require("http");
+    var path = require("path");
+    const chatRouter = require("./routes/chat")
 
-var app = express();
-app.set("views",path.join(__dirname,"views"));
-app.set("view engine","twig");
+    var app = express();
+    app.set("views",path.join(__dirname,"views"));
+    app.set("view engine","twig");
 
-const server = http.createServer(app);
+    const server = http.createServer(app);
 
-const io = require("socket.io")(server)
+    const io = require("socket.io")(server)
 
-app.use("/chat",chatRouter);
-io.on('connection', (socket)=> {
-    socket.on('chat message', msg => {
-        io.emit('chat message', msg);
-      });
-    console.log ('User Connected');
-    io.emit("msg","A new user has been connected !");
-    socket.on("disconnect",()=>{
-        io.emit("msg","A user disconnected");
-    })
+    app.use("/chat",chatRouter);
+    io.on('connection', (socket)=> {
+        socket.on('chat message', msg => {
+            io.emit('chat message', msg);
+        });
+        console.log ('User Connected');
+        io.emit("msg","A new user has been connected !");
+        socket.on("disconnect",()=>{
+            io.emit("msg","A user disconnected");
+        })
+        socket.on("keyup", msg =>{
+            io.emit('keyup',msg);
+        });
+
+    
     // socket.on("typing", data => {
        
     //     socket.emit("typing", data);
